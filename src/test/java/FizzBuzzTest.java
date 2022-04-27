7cb9b5895ad5a7e59882e891f59b56f9b1bf4467
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2022-04-27 18:07:06 -0300

    Dojo TDD Batch 78

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -10,3 +10,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Assert
  *
  */
+
+class FizzBuzzTest {
+
+    final FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    void whenMultipleOfThreeReturnFizz(){
+        String verifiedFizzBuzz = fizzBuzz.verifyFizzBuzz(3);
+
+        assertEquals("Fizz", verifiedFizzBuzz);
+    }
+
+    @Test
+    void whenMultipleOfFiveReturnBuzz() {
+        String verifiedFizzBuzz = fizzBuzz.verifyFizzBuzz(5);
+
+        assertEquals("Buzz", verifiedFizzBuzz);
+    }
+
+    @Test
+    void whenMultipleOfThreeAndFiveReturnFizzBuzz() {
+        String verifiedFizzBuzz = fizzBuzz.verifyFizzBuzz(15);
+
+        assertEquals("FizzBuzz", verifiedFizzBuzz);
+    }
+
+    @Test
+    void whenNotMultipleOfThreeAndFiveShouldReturnInputNumber(){
+        String verifiedFizzBuzz = fizzBuzz.verifyFizzBuzz(2);
+
+        assertEquals("2", verifiedFizzBuzz);
+    }
+
+}
